96d79db77682fa85697ec8b14ed9f41a1783665a
Maria Eduarda <133064900+MariaEduardaFB@users.noreply.github.com>
2024-01-29 16:42:39 -0300

    feat:dados metereológicos

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import requests
-import APIrequest
+from APIrequest import API_KEY
 
-def obter_imagems_satelites(API_KEY, latitude, longitude):
-    base_url = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+def obter_imagens_satelites(latitude, longitude):
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    parametros = {
+        'lat': latitude,
+        'lon': longitude,
+        'appid': API_KEY
+    }
+
+    resposta = requests.get(base_url, params=parametros)
+
+    # Verificar se a solicitação foi bem-sucedida (código de status HTTP 200)
+    if resposta.status_code == 200:
+        # Salvar a resposta, que contém dados meteorológicos, em um arquivo JSON ou processar conforme necessário
+        with open('dados_meteorologicos.json', 'w') as f:
+            f.write(resposta.text)
+    else:
+        print(f"Erro na solicitação: {resposta.status_code}")
+
+# Substitua 'SUA_CHAVE_API' pela chave da sua conta no OpenWeatherMap
+latitude = -15.0
+longitude = -45.0
+
+obter_imagens_satelites(latitude, longitude)
