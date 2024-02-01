ff85f010c4f10d0b09fe947502f10232105b2c3c
Maria Eduarda <133064900+MariaEduardaFB@users.noreply.github.com>
2024-01-31 22:05:08 -0300

    feat: climate api call done

--- APIrequest.py
+++ APIrequest.py
@@ -1,1 +1,15 @@
+import requests
+
 API_KEY = '5b21421170493bdae3c5059f07a9add7'
+
+city ='Iguatu'
+
+link = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=pt_br'
+
+requisicao = requests.get(link)
+requisicao_dic = requisicao.json()
+descricao = requisicao_dic['weather'][0]['description']
+temperatura = requisicao_dic['main']['temp'] - 273.15
+print(city)
+print(descricao)
+print(f'{temperatura}°C')
